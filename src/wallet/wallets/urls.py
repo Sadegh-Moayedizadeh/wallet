a0302c43e25c5a7d29524a442b626c9d4cbc1344
Sadegh <moayyedizadeh@mahsan.co>
2024-02-01 11:30:58 +0330

    Add homepage.

--- src/wallet/wallets/urls.py
+++ src/wallet/wallets/urls.py
@@ -3,7 +3,7 @@ from wallets.views import (CreateDepositView, CreateWalletView,
                            RetrieveWalletView, ScheduleWithdrawView)
 
 urlpatterns = [
-    path("", CreateWalletView.as_view()),
+    path("", CreateWalletView.as_view(), name="wallets"),
     path("<uuid>/", RetrieveWalletView.as_view()),
     path("<uuid>/deposit", CreateDepositView.as_view()),
     path("<uuid>/withdraw", ScheduleWithdrawView.as_view()),
